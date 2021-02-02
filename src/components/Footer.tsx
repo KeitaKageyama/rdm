dcc01ab78773044a7f5250817d2eed1e714cc7da
n-n-n-n <naoyukisato20@gmail.com>
2021-02-02 23:12:20 +0900

    SNSアイコンの追加

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,14 +4,18 @@ import {
   Text,
   Flex,
   Spacer,
+  Icon,
 } from "@chakra-ui/react";
 import React, { useState, useEffect, FC } from "react";
 import styled from "styled-components";
 import { Spacer as Space } from "src/components/Spacer/Spacer";
+import { IoLogoFacebook } from "react-icons/io"
+import { IoLogoTwitter } from "react-icons/io"
+import { IoLogoInstagram } from "react-icons/io"
 
 export const Footer: FC = () => {
   return (
-    <FooterBox bg="#222222" w="100%" h="242px" p="77px 50px" color="white">
+    <FooterBox bg="#222" w="100%" h="242px" p="77px 50px" color="white">
       <Flex>
         <Link>企業情報</Link>
         <Space size={20} horizontal/>
@@ -34,6 +38,14 @@ export const Footer: FC = () => {
         <Text>|</Text>
         <Space size={20} horizontal/>
         <Link>ランダム映画とは</Link>
+      </Flex>
+      <Space size={15}/>
+      <Flex>
+        <Icon as={IoLogoFacebook} />
+        <Space size={15} horizontal/>
+        <Icon as={IoLogoTwitter} />
+        <Space size={15} horizontal/>
+        <Icon as={IoLogoInstagram} />
       </Flex>
     </FooterBox>
   );
