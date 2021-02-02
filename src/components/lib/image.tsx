caa9764ace01770e337f36534f52f4587cae7eb2
keitakageyama <kogeyama3@gmail.com>
2021-02-02 17:01:06 +0900

    feat:画像コンポーネント作成

---
+++ src/components/lib/image.tsx
@@ -0,0 +1,27 @@
+import css from '@styled-system/css'
+import React, { FC } from 'react'
+import styled from 'styled-components'
+import {
+  color,
+  ColorProps,
+  layout,
+  LayoutProps,
+  position,
+  PositionProps,
+  space,
+  SpaceProps,
+  typography,
+  TypographyProps,
+} from 'styled-system'
+
+type StyledSystemProps = TypographyProps & SpaceProps & LayoutProps & ColorProps & PositionProps
+
+const styledSystemProps = [typography, space, layout, color, position]
+
+type ImageProps = StyledSystemProps & React.ImgHTMLAttributes<HTMLImageElement>
+
+export const Image: FC<ImageProps> = props => {
+  return <ImageRoot {...props}>{props.children}</ImageRoot>
+}
+
+const ImageRoot = styled('img')<StyledSystemProps>(css({}), styledSystemProps)
