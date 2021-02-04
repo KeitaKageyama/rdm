34e5a0cf1ef3744f3e9c0e156a733f3e20c47b5d
keitakageyama <kogeyama3@gmail.com>
2021-02-04 20:55:11 +0900

    feat:QR画面の全体作成

--- src/pages/verification/index.page.tsx
+++ src/pages/verification/index.page.tsx
@@ -1,7 +1,60 @@
-import React from "react";
-import { Text, View } from "src/components/lib";
+import React, { useState, useEffect, FC } from "react";
+import {
+  Flex,
+  Spacer,
+  Box,
+  Button,
+  Heading,
+  Text,
+  Image,
+} from "@chakra-ui/react";
+import styled from "styled-components";
+import { Spacer as Space } from "src/components/Spacer/Spacer";
+import { NextPage } from "next";
+import Link from "next/link";
+import { paths } from "src/constants/paths";
 
-const Verification = () => {
-  return <Text>QRなど確認ページ</Text>;
+const Verification: NextPage = () => {
+  return (
+    <ContentsArea>
+      <Space size={20} />
+      <Box
+        w="468px"
+        h="395px"
+        m="0 auto"
+        backgroundImage="url(./images/verification_bg.svg)"
+        backgroundRepeat="no-repeat"
+        backgroundPosition="center"
+      >
+        <Space size={150} />
+        <Heading color="#fff" fontSize="36px" textAlign="center">
+          ご購入ありがとうございます
+        </Heading>
+        <Text fontSize="18px" color="#fff" textAlign="center">
+          下記QRコードにて来場できますので大事に保管ください
+          <br />
+          このQRコードは送付メールより確認できます
+        </Text>
+      </Box>
+      <Space size={20} />
+
+      <Link href={paths.index}>
+        <Text
+          color="#fff"
+          textAlign="center"
+          textDecoration="underline"
+          cursor="pointer"
+        >
+          TOP画面へ戻る
+        </Text>
+      </Link>
+    </ContentsArea>
+  );
 };
+
+const ContentsArea = styled(Box)`
+  background: #505050;
+  min-height: 100vh;
+`;
+
 export default Verification;
