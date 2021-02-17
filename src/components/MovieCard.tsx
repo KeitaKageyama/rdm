2b79b1cd767695a99ae25236f4d27dc06e56edf4
n-n-n-n <naoyukisato20@gmail.com>
2021-02-17 16:17:32 +0900

    style: カーソルポインター追加

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,6 +1,10 @@
+import {
+  Box,
+} from "@chakra-ui/react";
 import { FC } from "react";
 import { paths } from "src/constants/paths";
 import Link from "next/link";
+import styled from "styled-components";
 
 type Props = {
   title: string;
@@ -15,14 +19,20 @@ export const MovieCard: FC<Props> = ({ ...props }) => {
     <Link
       href={{ pathname: paths.detail, query: { title, imagePath, overview } }}
     >
-      <div>
-        <img
-          src={`https://image.tmdb.org/t/p/w185/${imagePath}`}
-          alt="poster"
-          width="80%"
-        />
-        <p>{title}</p>
-      </div>
+      <MovieHover>
+        <div>
+          <img
+            src={`https://image.tmdb.org/t/p/w185/${imagePath}`}
+            alt="poster"
+            width="80%"
+          />
+          <p>{title}</p>
+        </div>
+      </MovieHover>
     </Link>
   );
 };
+
+const MovieHover = styled(Box)`
+ cursor : pointer;
+`
