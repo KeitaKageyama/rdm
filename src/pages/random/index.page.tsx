c6bba58c0fae72eeea891fcb22ddb04ed2d75919
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2021-02-22 08:19:55 +0900

    add: ランダム画面の見た目を途中まで作成した

--- src/pages/random/index.page.tsx
+++ src/pages/random/index.page.tsx
@@ -1,33 +1,57 @@
 import React from "react";
-import { Text, View } from "src/components/lib";
 import Link from "next/link";
 import { paths } from "src/constants/paths";
-import { Select } from "@chakra-ui/react";
+import { Box, Flex, Select, Heading, Text } from "@chakra-ui/react";
 
 const Random = () => {
   return (
-    <>
-      <Text>ランダムページ</Text>
-      <Link href={paths.payment}>支払いページへ</Link>
-      <Select
-        bg="#222"
-        borderColor="#222"
-        color="#FBFBFB"
-        placeholder="人数を選択"
-      ></Select>
-      <Select
-        bg="#222"
-        borderColor="#222"
-        color="#FBFBFB"
-        placeholder="日付を選択"
-      ></Select>
-      <Select
-        bg="#222"
-        borderColor="#222"
-        color="#FBFBFB"
-        placeholder="時間を選択"
-      ></Select>
-    </>
+    <Flex padding="8.125vw">
+      <Box flexGrow={1} w="50%" bg="#222" color="#FBFBFB">
+        <Flex flexDirection="column">
+          <Heading as="h2" fontSize="18px">
+            ランダム映画とは
+          </Heading>
+          <Box>
+            <Text>"全て"がランダムで決まる、新しい映画の楽しみ方です。</Text>
+            <Text>
+              なんとなく映画見に行きたいけど特に見たいものが決まっていない時、新しい映画を発掘したい時、映画を予約する手間が面倒な時、全てを運に委ねてみたい時…
+            </Text>
+            <Text>そんな時にランダム映画が役に立ちます。</Text>
+            <Text>
+              タイトルはもちろん値段・席・時間まで、ワンクリックで全て決まります。
+            </Text>
+            <Text>
+              ランダム映画を使って、貴方も普段と一味違う映画の楽しみ方を味わってはいかが？
+            </Text>
+          </Box>
+        </Flex>
+      </Box>
+      <Box flexGrow={1}>
+        <Flex flexDirection="column">
+          <Select
+            bg="#222"
+            borderColor="#222"
+            color="#FBFBFB"
+            placeholder="人数を選択"
+          ></Select>
+          <Select
+            bg="#222"
+            borderColor="#222"
+            color="#FBFBFB"
+            placeholder="日付を選択"
+          ></Select>
+          <Select
+            bg="#222"
+            borderColor="#222"
+            color="#FBFBFB"
+            placeholder="時間を選択"
+          ></Select>
+          <p>
+            <a href={paths.payment}>支払いページへ</a>
+          </p>
+        </Flex>
+      </Box>
+    </Flex>
   );
 };
 export default Random;
