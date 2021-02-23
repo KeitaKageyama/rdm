b78781f45c72470a7a3fffcf43f4f9c2e7e19778
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2021-02-23 18:01:25 +0900

    add: ランダム画面のスタイリングを途中まで

--- src/pages/random/index.page.tsx
+++ src/pages/random/index.page.tsx
@@ -1,28 +1,39 @@
 import React from "react";
 import Link from "next/link";
+import { NextPage } from "next";
 import { paths } from "src/constants/paths";
 import { Box, Flex, Select, Heading, Text } from "@chakra-ui/react";
+import styled from "styled-components";
 
-const Random = () => {
+const Random: NextPage = () => {
   return (
-    <Flex padding="8.125vw">
-      <Box flexGrow={1} w="50%" bg="#222" color="#FBFBFB">
+    <Flex marginX="7.84vw" marginY="24vh">
+      <Box
+        w="62.8%"
+        bg="#222"
+        color="#FBFBFB"
+        marginRight="8.3%"
+        paddingX="30px"
+        paddingY="26px"
+      >
         <Flex flexDirection="column">
-          <Heading as="h2" fontSize="18px">
+          <Heading as="h2" paddingBottom="32px" fontSize="18px">
             ランダム映画とは
           </Heading>
           <Box>
-            <Text>"全て"がランダムで決まる、新しい映画の楽しみ方です。</Text>
-            <Text>
+            <Description>
+              "全て"がランダムで決まる、新しい映画の楽しみ方です。
+            </Description>
+            <Description>
               なんとなく映画見に行きたいけど特に見たいものが決まっていない時、新しい映画を発掘したい時、映画を予約する手間が面倒な時、全てを運に委ねてみたい時…
-            </Text>
-            <Text>そんな時にランダム映画が役に立ちます。</Text>
-            <Text>
+            </Description>
+            <Description>そんな時にランダム映画が役に立ちます。</Description>
+            <Description>
               タイトルはもちろん値段・席・時間まで、ワンクリックで全て決まります。
-            </Text>
-            <Text>
+            </Description>
+            <Description>
               ランダム映画を使って、貴方も普段と一味違う映画の楽しみ方を味わってはいかが？
-            </Text>
+            </Description>
           </Box>
         </Flex>
       </Box>
@@ -54,4 +65,9 @@ const Random = () => {
     </Flex>
   );
 };
+
+const Description = styled(Text)`
+  padding-bottom: 18px;
+`;
+
 export default Random;
